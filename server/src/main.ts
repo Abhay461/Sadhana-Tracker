2816b4b09df3e934e82e160de57a7bc04a52a8e8
Abhay461 <abhaykumarsalempur8521@gmail.com>
2026-09-12 13:05:14 +0530

    Fix helmet import in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 import * as express from 'express';
 import { join } from 'path';
 import { AppModule } from './app.module';
